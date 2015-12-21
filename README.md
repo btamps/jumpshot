d26452f9986530d8029046614a7cb72baedfd773
Billy Tamplin <billytamplin@gmail.com>
2015-12-20 19:06:06 -0800

    added features to the readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # JumpShot
 
-A Meteor JS app using the Dribbble API.
+A Meteor JS app using the [Dribbble API](http://developer.dribbble.com/).
+
+## Features
+- Sign in using Dribbble authentication
+- Schedule future shots automatically
+- Post to user's account 
+- Drag and Drop images from the user's desktop
--- app/server/publish.js
+++ app/server/publish.js
@@ -2,10 +2,14 @@
 
 
 Meteor.publish('shots', function () {
+  if(!this.userId) return this.ready();
+
   return Shots.find({userId: this.userId});
 });
 
 Meteor.publish('shot', function (id) {
+  if(!this.userId) return this.ready();
+
   var shot = Shots.findOne({_id: id});
 
   return [
@@ -15,10 +19,13 @@ Meteor.publish('shot', function (id) {
 });
 
 Meteor.publish('users', function () {
+  if(!this.userId) return this.ready();
   return Meteor.users.find({}, {fields: {profile: 1}});
 });
 
 Meteor.publish('user', function (userId) {
+  if(!this.userId) return this.ready();
+
   return [
     Meteor.users.find({_id: userId}, {fields: {profile: 1}}),
     Shots.find({userId: userId})
